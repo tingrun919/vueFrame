8800e0d76104e029118545f14bb268d1be3a8b47
chikinlee <lizijian365@163.com>
2019-04-03 17:09:27 +0800

    add husky standard

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
-      .set('_c', resolve('src/components'));
+      .set('_c', resolve('src/components'))
   },
   devServer: {
     before: mockdata
